17a3350f765ea5c1004ca208e43c4d589d1f5155
Chris Mutel <cmutel@gmail.com>
2023-09-23 22:09:25 +0200

    Add functions for local and remote mappings

--- ecoinvent_interface/__init__.py
+++ ecoinvent_interface/__init__.py
@@ -5,10 +5,12 @@ __all__ = [
     "__version__",
     "EcoinventProcess",
     "ProcessFileType",
+    "ProcessMapping",
 ]
 
 __version__ = "2.0.dev1"
 
 from .core import EcoinventInterface, ReleaseType
+from .mapping import ProcessMapping
 from .process_interface import EcoinventProcess, ProcessFileType
 from .storage import CachedStorage
