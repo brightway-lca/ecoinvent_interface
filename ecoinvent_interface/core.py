brightway-lca/ecoinvent_interface

commit 08d82a82e319e7e67ea602116f8a938d79430ae2

More 3.8 compatibility fixes

ecoinvent_interface/core.py
@@ -127,7 +127,7 @@ class InterfaceBase:
         headers = {
             "ecoinvent-api-client-library": "ecoinvent_interface",
             "ecoinvent-api-client-library-version": __version__,
-        } | self.custom_headers
+        }.update(self.custom_headers)
         response = requests.post(sso_url, post_data, headers=headers, timeout=20)
 
         if response.ok:
@@ -148,7 +148,7 @@ class InterfaceBase:
             "Authorization": f"Bearer {self.access_token}",
             "ecoinvent-api-client-library": "ecoinvent_interface",
             "ecoinvent-api-client-library-version": __version__,
-        } | self.custom_headers
+        }.update(self.custom_headers)
         message = """Requesting URL.
     URL: {reports_url}
     Class: {self.__class__.__name__}
@@ -166,7 +166,7 @@ class InterfaceBase:
             "Authorization": f"Bearer {self.access_token}",
             "ecoinvent-api-client-library": "ecoinvent_interface",
             "ecoinvent-api-client-library-version": __version__,
-        } | self.custom_headers
+        }.update(self.custom_headers)
         message = """Requesting URL.
     URL: {files_url}
     Class: {self.__class__.__name__}
@@ -254,7 +254,7 @@ class InterfaceBase:
             "Authorization": f"Bearer {self.access_token}",
             "ecoinvent-api-client-library": "ecoinvent_interface",
             "ecoinvent-api-client-library-version": __version__,
-        } | self.custom_headers
+        }.update(self.custom_headers)
         self._streaming_download(
             url=url,
             params=params,
@@ -273,7 +273,7 @@ class InterfaceBase:
             "Authorization": f"Bearer {self.access_token}",
             "ecoinvent-api-client-library": "ecoinvent_interface",
             "ecoinvent-api-client-library-version": __version__,
-        } | self.custom_headers
+        }.update(self.custom_headers)
         s3_link = requests.get(url, headers=headers, timeout=20).json()["download_url"]
         self._streaming_download(
             url=s3_link, params={}, directory=directory, filename=filename

ecoinvent_interface/process_interface.py
@@ -148,7 +148,7 @@ class EcoinventProcess(InterfaceBase):
             "Authorization": f"Bearer {self.access_token}",
             "ecoinvent-api-client-library": "ecoinvent_interface",
             "ecoinvent-api-client-library-version": __version__,
-        } | self.custom_headers
+        }.update(self.custom_headers)
         message = """Requesting URL.
     URL: {url}
     Class: {self.__class__.__name__}
@@ -189,7 +189,7 @@ class EcoinventProcess(InterfaceBase):
             "Authorization": f"Bearer {self.access_token}",
             "ecoinvent-api-client-library": "ecoinvent_interface",
             "ecoinvent-api-client-library-version": __version__,
-        } | self.custom_headers
+        }.update(self.custom_headers)
         if meta.get("type").lower() == "xml":
             headers["Accept"] = "text/plain"
 

commit 967db21c2fd628bb8d0fefe2fe004a73e4d00677

Fix nonsense from 08d82a8 which broke everything

ecoinvent_interface/core.py
@@ -127,7 +127,8 @@ class InterfaceBase:
         headers = {
             "ecoinvent-api-client-library": "ecoinvent_interface",
             "ecoinvent-api-client-library-version": __version__,
-        }.update(self.custom_headers)
+        }
+        headers.update(self.custom_headers)
         response = requests.post(sso_url, post_data, headers=headers, timeout=20)
 
         if response.ok:
@@ -148,7 +149,8 @@ class InterfaceBase:
             "Authorization": f"Bearer {self.access_token}",
             "ecoinvent-api-client-library": "ecoinvent_interface",
             "ecoinvent-api-client-library-version": __version__,
-        }.update(self.custom_headers)
+        }
+        headers.update(self.custom_headers)
         message = """Requesting URL.
     URL: {reports_url}
     Class: {self.__class__.__name__}
@@ -166,7 +168,8 @@ class InterfaceBase:
             "Authorization": f"Bearer {self.access_token}",
             "ecoinvent-api-client-library": "ecoinvent_interface",
             "ecoinvent-api-client-library-version": __version__,
-        }.update(self.custom_headers)
+        }
+        headers.update(self.custom_headers)
         message = """Requesting URL.
     URL: {files_url}
     Class: {self.__class__.__name__}
@@ -254,7 +257,8 @@ class InterfaceBase:
             "Authorization": f"Bearer {self.access_token}",
             "ecoinvent-api-client-library": "ecoinvent_interface",
             "ecoinvent-api-client-library-version": __version__,
-        }.update(self.custom_headers)
+        }
+        headers.update(self.custom_headers)
         self._streaming_download(
             url=url,
             params=params,
@@ -273,7 +277,8 @@ class InterfaceBase:
             "Authorization": f"Bearer {self.access_token}",
             "ecoinvent-api-client-library": "ecoinvent_interface",
             "ecoinvent-api-client-library-version": __version__,
-        }.update(self.custom_headers)
+        }
+        headers.update(self.custom_headers)
         s3_link = requests.get(url, headers=headers, timeout=20).json()["download_url"]
         self._streaming_download(
             url=s3_link, params={}, directory=directory, filename=filename

ecoinvent_interface/process_interface.py
@@ -148,7 +148,8 @@ class EcoinventProcess(InterfaceBase):
             "Authorization": f"Bearer {self.access_token}",
             "ecoinvent-api-client-library": "ecoinvent_interface",
             "ecoinvent-api-client-library-version": __version__,
-        }.update(self.custom_headers)
+        }
+        headers.update(self.custom_headers)
         message = """Requesting URL.
     URL: {url}
     Class: {self.__class__.__name__}
@@ -189,7 +190,8 @@ class EcoinventProcess(InterfaceBase):
             "Authorization": f"Bearer {self.access_token}",
             "ecoinvent-api-client-library": "ecoinvent_interface",
             "ecoinvent-api-client-library-version": __version__,
-        }.update(self.custom_headers)
+        }
+        headers.update(self.custom_headers)
         if meta.get("type").lower() == "xml":
             headers["Accept"] = "text/plain"
 